23bf165e098dfcea017d22489f716addfa931bad
dtmiranda <hortadito2@gmail.com>
2022-05-14 19:15:57 -0100

    fix the reverse right and left car turn of

--- car.js
+++ car.js
@@ -41,14 +41,20 @@ class Car{
       this.speed = 0;
     }
 
+    if(this.speed != 0){
+      const flip = this.speed > 0 ? 1 : -1;
 
-    if(this.controls.left){
-      this.angle += 0.03;
+      if(this.controls.left){
+        this.angle += 0.03 * flip;
+      }
+  
+      if(this.controls.right){
+        this.angle -= 0.03 * flip;
+      }
     }
 
-    if(this.controls.right){
-      this.angle -= 0.03;
-    }
+
+    
     
     this.xPosition -= Math.sin(this.angle)*this.speed;
     this.yPosition -= Math.cos(this.angle)*this.speed;
